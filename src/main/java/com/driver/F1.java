9edc63cd1b9c4c43a0604050ccc07a7c40fa011c
RAMEES K H <khramees.ramees@gmail.com>
2023-01-28 13:06:42 +0530

    F1.java updated:- added contructor and change gear function

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,10 +4,12 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
+        super(name,4,0,8,false,"type",1);
     }
 
     public void accelerate(int rate){
         int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        newSpeed = this.getCurrentSpeed()+rate;
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -20,11 +22,37 @@ public class F1 extends Car {
 
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
+            this.stop();
+            this.setCurrentGear(1);
         }
         //for all other cases, change the gear accordingly
+        else if(newSpeed >=1 && newSpeed<=50) {
+            this.setCurrentSpeed(newSpeed);
+            this.setCurrentGear(1);
+        }
+        else if(newSpeed >=51 && newSpeed<=100) {
+            this.setCurrentSpeed(newSpeed);
+            this.setCurrentGear(2);
+        }
+        else if(newSpeed >=101 && newSpeed<=150) {
+            this.setCurrentSpeed(newSpeed);
+            this.setCurrentGear(3);
+        }
+        else if(newSpeed >=151 && newSpeed<=200) {
+            this.setCurrentSpeed(newSpeed);
+            this.setCurrentGear(4);
+        }
+        else if(newSpeed >=201 && newSpeed<=250) {
+            this.setCurrentSpeed(newSpeed);
+            this.setCurrentGear(5);
+        }
+        else if(newSpeed >250) {
+            this.setCurrentSpeed(newSpeed);
+            this.setCurrentGear(6);
+        }
 
         if(newSpeed > 0) {
-            changeSpeed(newSpeed, getCurrentDirection());
+            this.changeSpeed(newSpeed, getCurrentDirection());
         }
     }
 }
