0d0393147540d5824e8f68fa500dc603efaf836f
RAMEES K H <khramees.ramees@gmail.com>
2023-01-28 12:45:48 +0530

    added contructor and getter and setter inside Vehicle.java

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -10,7 +10,9 @@ public class Car extends Vehicle {
     private int seats;
 
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
+
         //Hint: Car extends Vehicle
+
     }
 
     public void changeGear(int newGear){
